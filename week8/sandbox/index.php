f878aacc14486e9b466b34bc39e8f2310bc7539c
Trevor Pels <tpels@stanford.edu>
2012-11-14 15:06:12 -0800

    Pretty sure I'm done with lab 8

--- week8/sandbox/index.php
+++ week8/sandbox/index.php
@@ -1,3 +1,12 @@
+<?php
+include 'Mobile_Detect.php';
+$detect = new Mobile_Detect();
+
+if (!$detect->isMobile()) {
+	header("Location: mobiletest/iphone.php?url=stanford.edu/~tpels/cgi-bin/week8/sandbox/mobile.php");
+}
+?>
+
 <!DOCTYPE html> 
 <html>
 
